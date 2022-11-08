c91f0c6d596d364a009c161930ef949b01438589
79181 <katerina.tokan@yandex.ru>
2022-11-09 02:18:17 +0300

    Сделано ДЗ5

--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,52 @@ public class Main {
     public static void main(String[] args) {
         // Задание 1
         System.out.println(" Задание 1 ");
-        int clientOS = 0;
-        int clientAd = 1;
+        int clientOS = 1;
+        int clientAd = 0;
         int client = 0;
-        if ( client != clientAd )
-
-        if ( 0 != 1 ) {
-            System.out.println(" Установите версию приложения для iOS по ссылке ");
-        }
-            if (1 != 0) {
+            if (client != 0) {
+                System.out.println(" Установите версию приложения для iOS по ссылке ");
+            } else {
                 System.out.println("Установите версию приложения для Android по ссылке ");
             }
-            // Задание 2
-            System.out.println(" Задание 2 ");
-            int clientDeviceYear = 2015;
-            int earOfReleaseClientOS = 2016;
-            int earOfReleaseClientAd = 2016;
-        if (earOfReleaseClientOS < clientDeviceYear && client == clientOS) {
-                System.out.println(" Установите облегченную версию приложения для iOS по ссылке ");
+        // Задание 2
+        System.out.println(" Задание 2 ");
+        int clientDeviceYear = 2013;
+        if (clientOS == 0){
+            if (clientDeviceYear >= 2015 ) {
+                System.out.println(" Установите  версию приложения для iOS по ссылке ");
             } else {
-            System.out.println(" Установите версию приложения для iOS по ссылке ");
-        }
-            if (earOfReleaseClientAd < clientDeviceYear ) {
-                System.out.println(" Установите облегченную версию приложения для Android по ссылке ");
-            }else{
-                System.out.println(" Установите версию приложения для Android  по ссылке ");
+                System.out.println(" Установите облегченную версию приложения для iOS по ссылке ");
             }
-            // Задание 3
-                        System.out.println(" Задание 3 " );
-                    int year = 2021;
-                    if ( (  year % 400 == 0) ||(( year % 4 == 0) && ( year % 100 != 0 ))) {
-                            System.out.println( year + " год является високосным ");
-                    }else{
-                        System.out.println( year + " год не является високосным");
-                    }
-            // Задание 4
-            System.out.println(" Задание 4 " );
-                    int deliveryDistance = 95;
-                    if ( deliveryDistance <= 20)
-                        System.out.println(" Потребуется 1 день ");
-            if ( (deliveryDistance > 20) && ( deliveryDistance <= 60))
-                System.out.println(" Потребуется 2 дня ");
-            if (( deliveryDistance >= 60) && (deliveryDistance <= 100))
-                System.out.println(" Потребуется 3 дня ");
-            // Задание 5
-            System.out.println(" Задание 5 " );
-            int monthNumber = 7 ;
+            }else if (clientOS == 1) {
+        if (clientDeviceYear >= 2015 ) {
+                System.out.println(" Установите версию приложения для Android по ссылке ");
+            } else {
+                System.out.println(" Установите облегченную версию приложения для Android  по ссылке ");
+            }
+        }
+
+
+        // Задание 3
+        System.out.println(" Задание 3 ");
+        int year = 2021;
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            System.out.println(year + " год является високосным ");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+        // Задание 4
+        System.out.println(" Задание 4 ");
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20)
+            System.out.println(" Потребуется 1 день ");
+        if ((deliveryDistance > 20) && (deliveryDistance <= 60))
+            System.out.println(" Потребуется 2 дня ");
+        if ((deliveryDistance >= 60) && (deliveryDistance <= 100))
+            System.out.println(" Потребуется 3 дня ");
+        // Задание 5
+        System.out.println(" Задание 5 ");
+        int monthNumber = 7;
 
             switch (monthNumber) {
                 case 1:
